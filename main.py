e9421cfaae049a0ca12e1dad5779a68d67d41a33
AbimanyuBP <70093819+AbimanyuBP@users.noreply.github.com>
2021-02-23 16:10:15 +0700

    File awal
    ini file awal todo list dengan python

---
+++ main.py
@@ -0,0 +1,29 @@
+username = str("user")
+nama = str
+todo = []
+kondisi = []
+default = "Belum Selesai"
+finished = "Selesai"
+p = 0
+
+while nama != username:
+    nama = input("Username anda:")
+    if nama == username:
+        print("Selamat datang " + nama)
+        print("="*50)
+        print("Todo anda saat ini adalah " + str(len(todo)) + " ingin menambahkan?")
+        respon = input("Response y/n? : ")
+        while respon == "y":
+            list = input("Apa ToDo nya ")   
+            todo.append(list)
+            kondisi.append(default)
+            respon = input("Mau tambah lagi y/n? ")
+        print("List yang terbuat adalah ")
+        while p < len(todo):
+            print(str(p+1) + " " + todo[p] + " : " + kondisi[p])
+            p += 1
+        break
+    else:
+        print("Username Tidak Ada")
+        continue
+    print('pw salah masukan lagi')
